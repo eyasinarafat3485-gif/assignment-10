c8ad8acd8a94dc8aaf3e264861ac2d0bde65be10
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-06-20 09:46:42 +0600

    my-requests page before code replace

--- src/app/dashboard/donor/my-requests/page.jsx
+++ src/app/dashboard/donor/my-requests/page.jsx
@@ -1,23 +1,3 @@
-// 'use client'
-// import ProfilePage from '@/components/dashboard/ProfilePage';
-// import { useSession } from '@/lib/auth-client';
-// import React from 'react';
-
-// const DonorMyRequestsPage = () => {
-//     const { data: session, isPending } = useSession();
-//     const user = session?.user;
-//     console.log(user, isPending);
-//     return (
-//         <div>
-//             <h2 className='text-xl text-red-500 font-bold mt-0 mr-0 text-right uppercase'>
-//                 {user?.role}
-//             </h2>
-//         </div>
-//     );
-// };
-
-// export default DonorMyRequestsPage;
-
 
 // 'use client';
 
