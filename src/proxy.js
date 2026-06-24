045e6258d647ab8115b36147d4545d5d23d7fa2e
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-06-24 13:30:42 +0600

    proxy protect route done

---
+++ src/proxy.js
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server'
+import { headers } from 'next/headers'
+import { auth } from '@/lib/auth'
+ 
+export async function proxy(request) {
+    const { pathname } = request.nextUrl
+
+    const session = await auth.api.getSession({
+        headers: await headers(),
+        // request: request 
+    })
+    console.log(session);
+    
+    console.log("Current Path:", pathname, "Session Found:", !!session);
+
+    if (!session) {
+        if (pathname !== '/login') {
+            return NextResponse.redirect(new URL('/login', request.url))
+        }
+    }
+
+    return NextResponse.next()
+}
+ 
+export const config = {
+  matcher: [
+    '/dashboard/:path*', 
+    '/search-donor', 
+    '/funding', 
+    '/donation-requests/:path*' 
+  ],
+}
