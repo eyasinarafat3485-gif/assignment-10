ac8f60bc705096efea39705f20d8f68cec63d773
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-06-26 15:37:21 +0600

    environmet file check

--- src/lib/actions/allBloods.js
+++ src/lib/actions/allBloods.js
@@ -1,19 +1,4 @@
 
-// 'use server'
-
-// import { serverMutationBReq, serverMutationDelete } from "../core/server"
-
-// export const createBloodRequests = async(newBloodRequests)=>{
-//     return serverMutationBReq('/api/bloodRequests', newBloodRequests);
-// }
-
-// export const deleteBloodRequest = async (id) => {
-//     return serverMutationDelete(`/api/bloodRequests/${id}`);
-// }
-
-
-
-
 'use server'
 
 import { serverMutationBReq, serverMutationDelete } from "../core/server"
